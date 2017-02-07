a395ee18b7d987a36f8a1d44aebef3fc74d7c3b1
zhiminghan <619341911@qq.com>
2017-02-08 02:13:01 +0800

    测试结果没有乱码

--- TestGit/src/Hello.java
+++ TestGit/src/Hello.java
@@ -1,9 +1,10 @@
-````````````
+
 public class Hello {
    public static void main(String[] args) {
 	
 	   System.out.println("Hello Git");
 	   System.out.println("你好 Git");
+	   System.out.println("谢谢 Git");
 	   
 }
 }
