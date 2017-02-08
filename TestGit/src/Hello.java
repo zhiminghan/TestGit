ae69d3b57bb1adf8c053be8fccac3a6622a23fa2
zhiminghan <619341911@qq.com>
2017-02-09 00:20:51 +0800

    添加注释

--- TestGit/src/Hello.java
+++ TestGit/src/Hello.java
@@ -2,7 +2,8 @@
 public class Hello {
    public static void main(String[] args) {
 	
-	   System.out.println("Hello Git");
+	   //添加一行注释	
+           System.out.println("Hello Git");
 	   System.out.println("你好 Git");
 	   System.out.println("谢谢 Git");
 	   
